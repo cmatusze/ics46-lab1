3bc35139228e0804cd474fc84fbc085ceb64d3b0
ct <cmatusze@uci.edu>
2025-10-17 18:09:46 -0700

    add files for lab1

---
+++ build/CMakeFiles/intro_lab.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tyya2021/Desktop/ICS46_F25/Introduction_Lab_cpp/src/Bag.cpp" "CMakeFiles/intro_lab.dir/src/Bag.cpp.o" "gcc" "CMakeFiles/intro_lab.dir/src/Bag.cpp.o.d"
+  "/Users/tyya2021/Desktop/ICS46_F25/Introduction_Lab_cpp/src/Token.cpp" "CMakeFiles/intro_lab.dir/src/Token.cpp.o" "gcc" "CMakeFiles/intro_lab.dir/src/Token.cpp.o.d"
+  "/Users/tyya2021/Desktop/ICS46_F25/Introduction_Lab_cpp/src/main.cpp" "CMakeFiles/intro_lab.dir/src/main.cpp.o" "gcc" "CMakeFiles/intro_lab.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Bag.cpp
@@ -0,0 +1,49 @@
+#include "Bag.hpp"
+#include <iostream>
+
+/**
+ * A Bag is a collection of Tokens (chips) that supports
+ * functions that work by enumerating the contents.
+ *
+ * @author: YOUR NAME HERE
+ */
+
+Bag::Bag(int numTokens) : contents(numTokens) {}
+
+void Bag::firstToken() const {
+    if (!contents.empty())
+        std::cout << contents[0].toString() << std::endl;
+}
+
+void Bag::allTokens() const {
+    for (const auto& token : contents) {
+        // TODO: print out the currently chosen Token
+        
+    }
+}
+
+void Bag::allTokensWhile() const {
+    size_t i = 0;
+    // TODO: turn this into a while loop that enumerates and
+    // prints all of the tokens in the bag
+    while () {
+        
+    }
+}
+
+int Bag::addTokens() const {
+    // TODO: write a loop that enumerates the entire bag
+	// and sums the total of all of the Token values
+    return 0;
+}
+
+int Bag::highValueTokens() const {
+    // TODO: write a loop that enumerates the entire bag
+    // and returns the number of high value Tokens
+    return 0;	
+}
+
+int Bag::firstGreen() const {
+    // TODO: search bag for a green Token, if found, return its index
+    return -1;
+}
